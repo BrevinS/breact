0453fd4a8d7b6a18a42854779d4ca93472904855
BrevinS <brevinsimon0@gmail.com>
2022-11-07 14:17:08 -0800

    Added animation

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from "react"
 import LandingPage from './LandingPage'
+import ThreeJS from './ThreeJS'
 
 function App() {
+
+
   return (
     <>
-    <LandingPage />
+    <ThreeJS />
     </>
   )
 }
